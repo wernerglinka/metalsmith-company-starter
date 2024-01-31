1e26c4c2c746ab8d8ed7109efed5c9d7ec9d1668
Werner Glinka <werner@glinka.co>
2024-01-31 12:01:27 -0800

    updated dependencies and refactored metalsmith build process

--- metalsmith.js
+++ metalsmith.js
@@ -1,126 +1,167 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-const Metalsmith = require('metalsmith');
-const markdown = require('@metalsmith/markdown');
-const layouts = require('@metalsmith/layouts');
-const collections = require('@metalsmith/collections');
-const drafts = require('@metalsmith/drafts');
-const permalinks = require('@metalsmith/permalinks');
-const sass = require('@metalsmith/sass');
-const postcss = require('@metalsmith/postcss');
-const when = require('metalsmith-if');
-const htmlMinifier = require('metalsmith-html-minifier');
-const assets = require('metalsmith-static-files');
-const metadata = require('@metalsmith/metadata');
-const prism = require('metalsmith-prism');
+import { performance } from 'perf_hooks';
+import browserSync from 'browser-sync';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import Metalsmith from 'metalsmith';
+import markdown from '@metalsmith/markdown';
+import layouts from '@metalsmith/layouts';
+import collections from '@metalsmith/collections';
+import drafts from '@metalsmith/drafts';
+import permalinks from '@metalsmith/permalinks';
+import sass from '@metalsmith/sass';
+import postcss from '@metalsmith/postcss';
+import when from 'metalsmith-if';
+import htmlMinifier from 'metalsmith-html-minifier';
+import assets from 'metalsmith-static-files';
+import metadata from '@metalsmith/metadata';
+import prism from 'metalsmith-prism';
 
-const marked = require('marked');
-const esbuild = require('@metalsmith/js-bundle');
+import * as marked from 'marked';
+import esbuild from '@metalsmith/js-bundle';
 
-const { dependencies } = require('./package.json');
+// ESM does not currently import JSON modules by default.
+// Ergo we'll JSON.parse the file manually
+import * as fs from 'fs';
 
+const { dependencies } = JSON.parse( fs.readFileSync( './package.json' ) );
+
+/* eslint-disable no-underscore-dangle */
+const __dirname = dirname( fileURLToPath( import.meta.url ) );
 const isProduction = process.env.NODE_ENV === 'production';
 
 // functions to extend Nunjucks environment
-const spaceToDash = (string) => string.replace(/\s+/g, '-');
-const condenseTitle = (string) => string.toLowerCase().replace(/\s+/g, '');
-const UTCdate = (date) => date.toUTCString('M d, yyyy');
-const blogDate = (string) =>
-  new Date(string).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-const trimSlashes = (string) => string.replace(/(^\/)|(\/$)/g, '');
-const mdToHTML = (mdString) => {
+const spaceToDash = ( string ) => string.replace( /\s+/g, '-' );
+const condenseTitle = ( string ) => string.toLowerCase().replace( /\s+/g, '' );
+const UTCdate = ( date ) => date.toUTCString( 'M d, yyyy' );
+const blogDate = ( string ) =>
+  new Date( string ).toLocaleString( 'en-US', { year: 'numeric', month: 'long', day: 'numeric' } );
+const trimSlashes = ( string ) => string.replace( /(^\/)|(\/$)/g, '' );
+const mdToHTML = ( mdString ) => {
   try {
-    return marked.parse(mdString);
-  } catch (e) {
+    return marked.parse( mdString );
+  } catch ( e ) {
     return mdString;
   }
 };
+const thisYear = () => new Date().getFullYear();
 
 // Define engine options for the inplace and layouts plugins
 const templateConfig = {
   directory: 'layouts',
   engineOptions: {
-    path: ['layouts'],
+    path: [ 'layouts' ],
     filters: {
       spaceToDash,
       condenseTitle,
       UTCdate,
       blogDate,
       trimSlashes,
-      mdToHTML
+      mdToHTML,
+      thisYear
     }
   }
 };
 
-Metalsmith(__dirname)
-  .source('./src/content')
-  .destination('./build')
-  .clean(true)
-  .metadata({
-    msVersion: dependencies.metalsmith,
-    nodeVersion: process.version
-  })
+let devServer = null;
+let t1 = performance.now();
 
-  .use(when(isProduction, drafts()))
+function msBuild() {
+  return (
+    Metalsmith( __dirname )
+      .clean( true )
+      .watch( isProduction ? false : [ 'src', 'layouts' ] )
+      .source( './src/content' )
+      .destination( './build' )
+      .clean( true )
+      .metadata( {
+        msVersion: dependencies.metalsmith,
+        nodeVersion: process.version
+      } )
 
-  .use(
-    metadata({
-      site: 'src/content/data/site.json',
-      nav: 'src/content/data/navigation.json'
-    })
-  )
+      .use( when( isProduction, drafts() ) )
 
-  .use(
-    collections({
-      blog: {
-        pattern: 'blog/*.md',
-        sortBy: 'date',
-        reverse: true,
-        limit: 10
-      }
-    })
-  )
+      .use(
+        metadata( {
+          site: 'src/content/data/site.json',
+          nav: 'src/content/data/navigation.json'
+        } )
+      )
 
-  .use(markdown())
+      .use(
+        collections( {
+          blog: {
+            pattern: 'blog/*.md',
+            sortBy: 'date',
+            reverse: true,
+            limit: 10
+          }
+        } )
+      )
 
-  .use(permalinks())
+      .use( markdown() )
 
-  .use(layouts(templateConfig))
+      .use( permalinks() )
 
-  .use(
-    prism({
-      lineNumbers: true,
-      decode: true
-    })
-  )
+      .use( layouts( templateConfig ) )
 
-  .use(
-    assets({
-      source: 'src/assets/',
-      destination: 'assets/'
-    })
-  )
+      .use(
+        prism( {
+          lineNumbers: true,
+          decode: true
+        } )
+      )
 
-  .use(
-    sass({
-      entries: {
-        'src/sass/styles.scss': 'assets/styles.css'
-      }
-    })
-  )
-  .use(postcss({ plugins: ['postcss-preset-env', 'autoprefixer', 'cssnano'], map: !isProduction }))
+      .use(
+        assets( {
+          source: 'src/assets/',
+          destination: 'assets/'
+        } )
+      )
 
-  .use(
-    esbuild({
-      entries: {
-        'assets/scripts': 'src/js/main.js'
-      }
-    })
-  )
+      .use(
+        sass( {
+          entries: {
+            'src/sass/styles.scss': 'assets/styles.css'
+          }
+        } )
+      )
+      .use( postcss( { plugins: [ 'postcss-preset-env', 'autoprefixer', 'cssnano' ], map: !isProduction } ) )
 
-  .use(when(isProduction, htmlMinifier()))
-  .build((err) => {
-    if (err) {
-      throw err;
+      .use(
+        esbuild( {
+          entries: {
+            'assets/scripts': 'src/js/main.js'
+          }
+        } )
+      )
+
+      .use( when( isProduction, htmlMinifier() ) )
+  );
+}
+
+const ms = msBuild();
+ms.build( ( err ) => {
+  if ( err ) {
+    throw err;
+  }
+  /* eslint-disable no-console */
+  console.log( `Build success in ${ ( ( performance.now() - t1 ) / 1000 ).toFixed( 1 ) }s` );
+  if ( ms.watch() ) {
+    if ( devServer ) {
+      t1 = performance.now();
+      devServer.reload();
+    } else {
+      devServer = browserSync.create();
+      devServer.init( {
+        host: 'localhost',
+        server: './build',
+        port: 3000,
+        injectChanges: false,
+        reloadThrottle: 0
+      } );
     }
-  });
+  }
+} );
+
